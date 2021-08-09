3d880d48837eb340b1eb24e500099ab9dec6959f
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-08-09 02:49:03 -0400

    updated to include

--- reactblog/server/models/Post.js
+++ reactblog/server/models/Post.js
@@ -2,23 +2,33 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const moment = require('moment-timezone');
+const dateUsa = moment.tz(Date.now(), "America/New_York");
+
+console.log(dateUsa);
 
 const postSchema = new Schema({
   title: {
     type: String,
     required: true,
     trim: true
-  },
-  date_created: {
-    type: Date,
-    required: true,
-    unique: true,
-    default: Date.now
-  },
+  },  
   content: {
     type: String,
     required: true,
   },
+  date_Created: {type: String, "default": moment.tz(Date.now(), "America/New_York")}
+  
+  
+});
+
+postSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+  if ( !this.created_at ) {
+    this.created_at = now;
+  }
+  next();
 });
 
 
