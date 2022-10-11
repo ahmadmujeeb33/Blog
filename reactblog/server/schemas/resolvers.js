fb4cd6b323b1d54c12a284fb9cec935f04ede909
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2022-10-10 20:22:43 -0400

    removed console.log

--- reactblog/server/schemas/resolvers.js
+++ reactblog/server/schemas/resolvers.js
@@ -14,27 +14,18 @@ const resolvers = {
         },
     
         user: async (parent, { userName }) => {
-          console.log("theoridfffffffffffsdfkljfasldkf")
-          console.log("userName " + userName);
           const userFound = await User.findOne({ userName: userName }).populate('posts');
           console.log("userfound " + userFound);
           return userFound
         },
         me: async (parent, args, context) => {
-          console.log("----------------")
-          console.log(context.user)
           if (context.user) {
-            console.log("in thisss")
-            console.log("_id " + context.user._id)
             return User.findOne({ _id: context.user._id }).populate('posts');
           }
           throw new AuthenticationError('You need to be logged in!');
         },
         post: async(parent, {_id}, context) =>{
-          console.log("+++++++++++++++++++++++++++++++++++++++++++++++")
-          console.log("_id " + _id);
           const post =  await Post.findOne({_id: _id}).populate('comments');
-          console.log(post);
           return post;
         }
     
@@ -42,7 +33,6 @@ const resolvers = {
 
     Mutation: {
         addUser: async (parent, { userName, email, password }) => {
-          console.log("-----------------------------------")
           const user = await User.create({ userName, email, password });
           const token = signToken(user);
     
@@ -61,7 +51,6 @@ const resolvers = {
             throw new AuthenticationError('Incorrect password!');
           }
 
-          console.log(user)
     
           const token = signToken(user);
           return { token, user };
@@ -69,18 +58,8 @@ const resolvers = {
 
         addPost: async (parent, { title, content,_id,date_Created}) => {
 
-          console.log("in here");
-
-          console.log("title " + title);
-          console.log("content " + content);
-          console.log("userId " + _id);
-          console.log("date_Created " + date_Created);
-
-          console.log("+=+=+=++++====++==++==++==")
-
           const postData = await Post.create({title,content,date_Created});
 
-          console.log(postData);
 
           await User.findOneAndUpdate(
             { _id: _id },
@@ -97,16 +76,7 @@ const resolvers = {
         },
 
         updatePost: async (parent, { title, content,_id,date_Created}) => {
-            console.log("------------------")
-
-            console.log("in here");
-
-            console.log("title " + title);
-            console.log("content " + content);
-            console.log("userId " + _id);
-            console.log("date_Created " + date_Created);
-
-
+           
             const updateData = await Post.findOneAndUpdate(
               { _id: _id },
               {
@@ -125,11 +95,8 @@ const resolvers = {
 
         deletePost: async (parent, {_id}, context) =>{
 
-          console.log("_id1 " + _id )
           const deleteData = await Post.deleteOne({_id:_id})
-          console.log("context.user" + context.user)
-          console.log("----------------------")
-          console.log("contezxt.fwuse " + context.user._id);
+         
           await User.findByIdAndUpdate(
 
             {_id:context.user._id},
@@ -142,8 +109,7 @@ const resolvers = {
         },
 
         addFollower: async(parent,args) =>{
-          console.log("Args._id " + args._id);
-          console.log("followers " + args.follower);
+         
           return await User.findOneAndUpdate(
             { _id: args._id },
             {
@@ -162,14 +128,7 @@ const resolvers = {
 
         addComment: async (parent, {content,_id,date_Created,userName}) => {
 
-          console.log("in here");
-
-          // console.log("title " + title);
-          console.log("content " + content);
-          console.log("userId " + _id);
-          console.log("date_Created " + date_Created);
-
-          console.log("+=+=+=++++====++==++==++==")
+         
 
           const commentData = await Comment.create({content,date_Created,userName});
 
